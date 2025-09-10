63e653343da1b738dd0d519702191d90a6f48728
Druidfiren <druidfiren@gmail.com>
2025-09-10 21:24:47 +0200

    cypress Argos

---
+++ cypress/e2e/argos_Workflow_40.cy.js
@@ -0,0 +1,23 @@
+describe("Add research intent", () => {
+  it("should fill form and submit successfully", () => {
+    cy.visit("https://argos.appnificent.cz/");
+
+    // Zachytíme POST request na submit formuláře
+    cy.intercept("PATCH", "**/approvers").as("submitIntent");
+
+    // Zvolíme 'Mé výzkumné záměry'
+    cy.contains("Workflows").click();
+
+    // Klikneme na schválit
+    cy.contains("Common Research Proposal Workflow").click();
+
+    cy.contains("i", "edit").click();
+
+    cy.get('input[type="number"]').clear().type("40");
+
+    cy.contains("span", "Uložit").click();
+
+    // Počkáme na request a ověříme status 200
+    cy.wait("@submitIntent").its("response.statusCode").should("eq", 204);
+  });
+});
